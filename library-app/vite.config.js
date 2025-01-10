e7f9b852f9dc60ed7698746c5403fa15867bdea0
Davies Ningthouja <meiteidavies@gmail.com>
2025-01-10 23:41:56 +0530

    update edit

--- library-app/vite.config.js
+++ library-app/vite.config.js
@@ -5,12 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.lottie'], 
+  build: {
+    outDir: 'dist', // Default Vite output folder
+    sourcemap: true, // Optional: Enable for debugging
+  },
   server: {
     host: '0.0.0.0', // Allows connections from external IPs
     port: 5173, // Replace with your desired port if different
     cors: true,
    },
-    build: {
-      sourcemap: true, // Optional: Enable source maps for debugging (set to false if not needed)
-    },
+   
 })
